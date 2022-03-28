d1323f023c5b281b2bc5e239be96f053c7640342
Ryan Clough <ryclough@paloaltonetworks.com>
2022-03-28 15:15:24 -0700

    Refactors

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,55 @@ const htmlmin = require('html-minifier');
 
 const EXCLUDED_TAGS = ['blog_posts'];
 
+const generateTopics = function(collectionApi) {
+  const tagsHash = {};
+  collectionApi.getAll().map(item => {
+    if (item.data.tags) {
+      item.data.tags.map((tag) => {
+
+        // Remove excluded tags
+        if (EXCLUDED_TAGS.includes(tag)) {
+          return;
+        }
+
+        // Initalize the tag count at 1
+        if (!Object.keys(tagsHash).includes(tag)) {
+          tagsHash[tag] = 1;
+          return;
+        }
+
+        // Increment the tag count
+        tagsHash[tag] += 1;
+      })
+    }
+  });
+
+  // Convert the tag hash into a sorted list
+  const sorted = [];
+  for (const [k, v] of Object.entries(tagsHash)) {
+    sorted.push([k, v]);
+  }
+  sorted.sort(function(a, b) {
+    return a[1] - b[1];
+  }).reverse();
+
+  console.log(`[info] Sorted Tags: ${sorted.join('|')}`);
+  return sorted;
+};
+
+const minify = function(content, outputPath) {
+  if (outputPath.endsWith('.html')) {
+    let minified  = htmlmin.minify(content, {
+      removeComments: true,
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true,
+    });
+    return minified;
+  }
+  return content;
+};
+
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('src/assets/');
@@ -10,54 +59,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/css/');
   eleventyConfig.addWatchTarget('src/css/');
 
-  eleventyConfig.addCollection('topics', function(collectionApi) {
-    const tagsHash = {};
-    collectionApi.getAll().map(item => {
-      if (item.data.tags) {
-        item.data.tags.map((tag) => {
-
-          // Remove excluded tags
-          if (EXCLUDED_TAGS.includes(tag)) {
-            return;
-          }
-
-          // Initalize the tag count at 1
-          if (!Object.keys(tagsHash).includes(tag)) {
-            tagsHash[tag] = 1;
-            return;
-          }
-
-          // Increment the tag count
-          tagsHash[tag] += 1;
-        })
-      }
-    });
-
-    // Convert the tag hash into a sorted list
-    const sorted = [];
-    for (const [k, v] of Object.entries(tagsHash)) {
-      sorted.push([k, v]);
-    }
-    sorted.sort(function(a, b) {
-      return a[1] - b[1];
-    }).reverse();
-
-    console.log(`[info] Sorted Tags: ${sorted.join('|')}`);
-    return sorted;
-  });
-
-  eleventyConfig.addTransform('htmlmin', function(content, outputPath) {
-    if (outputPath.endsWith('.html')) {
-      let minified  = htmlmin.minify(content, {
-        removeComments: true,
-        collapseWhitespace: true,
-        minifyCSS: true,
-        minifyJS: true,
-      });
-      return minified;
-    }
-    return content;
-  });
+  eleventyConfig.addCollection('topics', generateTopics);
+  eleventyConfig.addTransform('htmlmin', minify);
 
   return {
     dir: {
--- src/_includes/scripts/header.js
+++ src/_includes/scripts/header.js
@@ -2,4 +2,5 @@
 	let randInt = Math.floor(Math.random() * 3) + 1;
 	let img = document.getElementsByClassName('profile-pic')[0];
 	img.src = '/assets/images/profile-pic-' + randInt + '.jpg';
+	img.alt = 'Ryan Clough profile picture';
 })();
