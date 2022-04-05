7b61157dd0b415c6b184eb9c0a0554163f66e16f
Ryan Clough <ryclough@paloaltonetworks.com>
2022-04-04 22:23:01 -0700

    Update meta.js

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,12 +1,3 @@
-// module.exports = {
-//   NOTE: `process.env.URL` is provided by Netlify, and may need
-//   adjusted pending your host
-//   url: process.env.URL || "http://localhost:8080",
-//   siteName: "clough.cloud",
-//   siteDescription: "Threat Intelligence & Software Development Professional. Creator of too many threat map visualizations. Opinions are my own and not the views of my employer.",
-//   authorName: "Ryan Clough", // optional
-// };
-
 module.exports = function() {
   return {
     author: 'Ryan Clough',
